3611df623c3406ecea6f590b4fec6032b259521a
Ashley Lampard <ashleylampard@live.com>
2022-05-23 11:30:49 +0100

    completed dev env chat app creation

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -6,14 +6,28 @@ export default class extends Controller {
   static targets = ["messages"]
 
   connect() {
-    consumer.subscriptions.create(
+    this.channel = consumer.subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
       { received: (message) => {
         // this.element.insertAdjacentHTML('beforeend', message)
-        this.messagesTarget.insertAdjacentHTML("beforeend", message)
+        this.#insertMessageAndScrollDown(message)
       } }
       )
 
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
   }
 }
